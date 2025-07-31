c0fe8f523a8fb74863c4db93d83e52335d4fe2e1
Matheus Santos <69777235+msbdacosta@users.noreply.github.com>
2025-07-31 22:28:46 +0000

    Day 3 completed!

--- week-1/day-3/Database.sql
+++ week-1/day-3/Database.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE week1;
+\c week1;
+
 CREATE TABLE people (
  id SERIAL PRIMARY KEY,
  name TEXT,
@@ -6,9 +9,9 @@ CREATE TABLE people (
 );
 
 INSERT INTO people (name, age, country) VALUES
-("Matthew", 26, "Brazil"),
-("Aurora", 3, "Norway"),
-("Raquel", 26, "Brazil"),
-("Tsunoda", 26, "Japan"),
-("Felipe", 26, "Canada"),
-("Michael", 26, "Hawaii");
+('Matthew', 26, 'Brazil'),
+('Aurora', 3, 'Norway'),
+('Raquel', 26, 'Brazil'),
+('Tsunoda', 26, 'Japan'),
+('Felipe', 26, 'Canada'),
+('Michael', 26, 'Hawaii');
